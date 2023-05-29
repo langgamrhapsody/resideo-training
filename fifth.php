d9adc88822dee112fe7105d5e59f137e9f93d965
langgamrhapsody <66179653+langgamrhapsody@users.noreply.github.com>
2023-05-29 17:10:19 +0800

    fix fifth section, and complete fourth section

--- fifth.php
+++ fifth.php
@@ -2,7 +2,7 @@
         <div class="try__background">
             <div class="try__container container">
                 <div class="try__row row">
-                    <div class="try__item col-12 col-sm-7 col-md-6 col-lg-4">
+                    <div class="try__item col-12 col-sm-7 col-md-6 col-lg-5 col-xl-4">
                         <div class="try__content">
                             <div class="try__content-text">
                                 <h3 class="try__content-text-title">Search <span class="highlight-text">Smarter</span>,
--- fourth.php
+++ fourth.php
@@ -4,7 +4,7 @@
             <h3 class="featured-agents__content-title">Our Featured Agents</h3>
             <p class="featured-agents__content-desc">Meet the best real estate agents</p>
             <div class="featured-agents__content-row row">
-                <div class="featured-agents__content-item col-3">
+                <div class="featured-agents__content-item col-10 col-sm-6 col-lg-3">
                     <div class="featured-agents__content-item-card--first">
                         <a href="#" class="btn featured-agents__content-item-card-agent">
                             <p class="agent-name">Scott Baldwin</p>
@@ -19,14 +19,50 @@
                         </a>
                     </div>
                 </div>
-                <div class="featured-agents__content-item col-3">
-                    <div class="featured-agents__content-item-card--second"></div>
+                <div class="featured-agents__content-item col-10 col-sm-6 col-lg-3">
+                    <div class="featured-agents__content-item-card--second">
+                        <a href="#" class="btn featured-agents__content-item-card-agent">
+                            <p class="agent-name">Alayna Becker</p>
+                            <div class="agent-contact">
+                                <svg class="agent-contact__icon" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 512 512">
+                                    <path fill="currentColor"
+                                        d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                                </svg>
+                                <p class="agent-contact__number">(123) 456-7890</p>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                <div class="featured-agents__content-item col-3">
-                    <div class="featured-agents__content-item-card--third"></div>
+                <div class="featured-agents__content-item col-10 col-sm-6 col-lg-3">
+                    <div class="featured-agents__content-item-card--third">
+                        <a href="#" class="btn featured-agents__content-item-card-agent">
+                            <p class="agent-name">Melvin Blackwell</p>
+                            <div class="agent-contact">
+                                <svg class="agent-contact__icon" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 512 512">
+                                    <path fill="currentColor"
+                                        d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                                </svg>
+                                <p class="agent-contact__number">(123) 456-7890</p>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                <div class="featured-agents__content-item col-3">
-                    <div class="featured-agents__content-item-card--fourth"></div>
+                <div class="featured-agents__content-item col-10 col-sm-6 col-lg-3">
+                    <div class="featured-agents__content-item-card--fourth">
+                        <a href="#" class="btn featured-agents__content-item-card-agent">
+                            <p class="agent-name">Erika Tillman</p>
+                            <div class="agent-contact">
+                                <svg class="agent-contact__icon" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 512 512">
+                                    <path fill="currentColor"
+                                        d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                                </svg>
+                                <p class="agent-contact__number">(123) 456-7890</p>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
             <a href="#" class="featured-agents__content-button">SEE ALL AGENTS</a>
