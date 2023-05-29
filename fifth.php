0b0bbfeea14ea89b2ecbd4a52728d84ce3500e1a
langgamrhapsody <66179653+langgamrhapsody@users.noreply.github.com>
2023-05-29 13:24:26 +0800

    revision fifith section

--- fifth.php
+++ fifth.php
@@ -1,13 +1,19 @@
     <div class="try">
         <div class="try__background">
             <div class="try__container container">
-                <div class="try__content">
-                    <div class="try__content-text">
-                        <h3 class="try__content-text-title">Search Smarter, From Anywhere</h3>
-                        <p class="try__content-text-desc">Power your search with our Resideo real estate platform,
-                            for timely listings and a seamless experience</p>
+                <div class="try__row row">
+                    <div class="try__item col-4">
+                        <div class="try__content">
+                            <div class="try__content-text">
+                                <h3 class="try__content-text-title">Search <span class="highlight-text">Smarter</span>,
+                                    From Anywhere</h3>
+                                <p class="try__content-text-desc">Power your search with our Resideo real estate
+                                    platform,
+                                    for timely listings and a seamless experience</p>
+                            </div>
+                            <a class="try__content-button">SEARCH NOW</a>
+                        </div>
                     </div>
-                    <a class="try__content-button">SEARCH NOW</a>
                 </div>
             </div>
         </div>
